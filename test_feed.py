77ac81db519e12bb6ed4fe20936eba21c00a16d0
bdpedigo <benjamindpedigo@gmail.com>
2018-03-03 12:22:49 -0800

    exponential vib

--- test_feed.py
+++ test_feed.py
@@ -6,6 +6,18 @@ import numpy as np
 feed = libmyo.device_listener.Feed()
 hub = libmyo.Hub()
 hub.run(1000, feed)
+
+def send_vibe(param):
+    myo.vibrate("short")
+    #linear
+    #delay = param * (1.2/6.5) + 0.3
+    delay = 0.3 * np.exp(param)
+    time.sleep(delay)
+
+def angle_shift(angles, f, l, r)
+    angles = angles - f
+
+
 try:
     myo = feed.wait_for_single_device(timeout=5.0)  # seconds
     if not myo:
@@ -14,12 +26,23 @@ try:
 
     # initialization
     if hub.running and myo.connected:
-        print('Collecting initial orientation...')
-        print('Hold still')
-        time.sleep(5)
+        print('Get Ready')
+        time.sleep(3)
+        print('Hold arm straight')
+        time.sleep(3)
         baseline = myo.orientation
         baseline_rpy = np.array(baseline.rpy)
         print(baseline_rpy)
+        print()
+        print('Hold arm left')
+        time.sleep(3)
+        left_rpy = np.array(myo.orientation.rpy)
+        print(left_rpy)
+        print()
+        print('Hold arm right')
+        time.sleep(3)
+        right_rpy = np.array(myo.orientation.rpy)
+        print(right_rpy)
         print()
 
     while hub.running and myo.connected:
@@ -30,9 +53,7 @@ try:
         diff_mag = np.sum(diff_mag)
         diff_mag = np.sqrt(diff_mag)
         print(diff_mag)
-        myo.vibrate("short")
-        delay = diff_mag * (1.2/6.5) + 0.3
-        time.sleep(delay)
+        send_vibe(diff_mag)
 
 except KeyboardInterrupt:
     print("Quitting...")
